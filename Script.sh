0ce10c2d3506949a6ea3a8395b431fb8d4a47581
vyasgiridhar <vyasgiridhar27@gmail.com>
2015-12-17 15:41:40 +0530

    Simple Updates

--- Script.sh
+++ Script.sh
@@ -3,13 +3,12 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo add-apt-repository ppa:webupd8team/java
-wget http://dl.dropbox.com/u/11876059/DPO_RT3290_LinuxSTA_V2600_20120508.tar.gz
-tar -xvf DPO_RT3290_LinuxSTA_V2600_20120508.tar.gz
-cd ~/DPO_RT3290_LinuxSTA_V2600_20120508
-make
-sudo make install
-sudo modprobe rt3290sta
+
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install google-chrome-stable oracle-java8-installer oracle-java8-set-default youtube-dl ircii vlc geany
-
+sudo apt-get install google-chrome-stable oracle-java8-installer oracle-java8-set-default youtube-dl ircii vlc geany curl libgtkmm-3.0-dev conky git  
+git clone git://github.com/gmate/gmate.git
+cd gmate/
+./install.sh 
+sudo ./install.sh
+sudo apt-get install gedit-plugins lm-sensors
